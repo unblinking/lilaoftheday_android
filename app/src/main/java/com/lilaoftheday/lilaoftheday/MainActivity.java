4fbec86835c6ee555e539a9c624bc7fff24bfc5a
Joshua Gray <jmg1138@gmail.com>
2016-04-28 16:09:56 -0700

    MainActivity.java     - Now extends AppCompatActivity instead of Activity     - Stopped keeping a variable to hold Context     - Removed the AdMob code     - Removed unnecessary whitespace

--- app/src/main/java/com/lilaoftheday/lilaoftheday/MainActivity.java
+++ app/src/main/java/com/lilaoftheday/lilaoftheday/MainActivity.java
@@ -1,11 +1,10 @@
 package com.lilaoftheday.lilaoftheday;
 
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -13,85 +12,68 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toolbar;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
-    Context context;
     CatListAdapter catListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        context = getApplicationContext();
+        setContentView(R.layout.activity_main);
 
         // Initialize default preference values.
         PreferenceManager.setDefaultValues(
-                context,
+                getApplicationContext(),
                 R.xml.preferences,
                 false // only if this method has never been called in the past
         );
 
-        /* Using a toolbar as the actionbar */
+        // Create a toolbar.
         Toolbar toolbar;
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setActionBar(toolbar);
 
-        /* The awesome recycler view */
-        RecyclerView rv;
-        rv = (RecyclerView) findViewById(R.id.rv);
-        rv.setHasFixedSize(true);
+        // Create a recycler view.
+        RecyclerView recyclerView;
+        recyclerView = (RecyclerView) findViewById(R.id.rv);
+        recyclerView.setHasFixedSize(true);
         StaggeredGridLayoutManager staggeredGridLayoutManager;
         staggeredGridLayoutManager = new StaggeredGridLayoutManager(3, 1);
-        rv.setLayoutManager(staggeredGridLayoutManager);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
         catListAdapter = new CatListAdapter(this);
-        rv.setAdapter(catListAdapter);
-        rv.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(catListAdapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
     }
 
-    /* TODO: Uncomment at publication time with production ad unit ID
-    // Here is AdMob
-    AdView mAdView = (AdView) findViewById(R.id.adView);
-    AdRequest adRequest = new AdRequest.Builder().build();
-    mAdView.loadAd(adRequest);
-    */
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
-        /* Inflate the menu; add items to the tool bar */
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         /*
         Handle tool bar item clicks here. The tool bar will automatically handle clicks on the
         Home/Up button, so long as you specify a parent activity in AndroidManifest.xml.
         */
-
         int id = item.getItemId();
-
         if (id == R.id.action_preferences) {
             Intent intent = new Intent();
-            intent.setClassName(this, "com.lilaoftheday.lilaoftheday.PreferencesActivity");
+            String className = "com.lilaoftheday.lilaoftheday.PreferencesActivity";
+            intent.setClassName(getApplicationContext(), className);
             startActivity(intent);
             return true;
         }
-
         if (id == R.id.action_notification_check) {
             AlarmChecker alarmChecker;
             alarmChecker = new AlarmChecker();
-            alarmChecker.checkAlarm(context);
+            alarmChecker.checkAlarm(getApplicationContext());
         }
-
         return super.onOptionsItemSelected(item);
-
     }
 
 }
