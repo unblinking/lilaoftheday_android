b1ce2e91d3be907f0f51472b2d00f7b2c86ea096
Joshua <jmg1138@gmail.com>
2017-03-13 16:17:35 -0700

    Photo fits notification now
    With the same photo lightened and pinkened as the background space.

--- app/src/main/java/com/lilaoftheday/lilaoftheday/utilities/Utilities.java
+++ app/src/main/java/com/lilaoftheday/lilaoftheday/utilities/Utilities.java
@@ -2,6 +2,10 @@ package com.lilaoftheday.lilaoftheday.utilities;
 
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.LightingColorFilter;
+import android.graphics.Paint;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -26,6 +30,20 @@ public class Utilities {
                 return result;
             }
         }
+    }
+
+    public static Bitmap makeTintedBitmap(Bitmap src) {
+        Bitmap result = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+        Canvas c = new Canvas(result);
+        Paint paint = new Paint();
+        // Lighten the bitmap
+        paint.setColorFilter(new LightingColorFilter(0xffffff, 0xffaaaaaa));
+        c.drawBitmap(src, 0, 0, paint);
+        // Pinken the bitmap
+        Paint paint2 = new Paint();
+        paint2.setColorFilter(new LightingColorFilter(0xffc6ff, 0));
+        c.drawBitmap(result, 0, 0, paint2);
+        return result;
     }
 
 }
